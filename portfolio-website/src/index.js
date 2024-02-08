2b2413975c4843bc0a8665c503d83cca4f0deed4
Safal Shrestha <sshrestha24@wooster.edu>
2024-02-08 18:23:32 -0500

    navbar

--- portfolio-website/src/index.js
+++ portfolio-website/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<link href= 'https://fonts.google.com/specimen/EB+Garamond/about?query=EB+Ga' rel='stylesheet' type='text/css'></link>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
